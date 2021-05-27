978bcb8423deafc662b77de5030f8f41c86b6cf9
joaquin_eas@hotmail.com <joaquin_eas@hotmail.com>
2021-05-27 13:33:06 -0300

    formulario de creacion producto

--- app.js
+++ app.js
@@ -9,15 +9,22 @@ app.listen(process.env.PORT || 3000,()=>{
 app.use(express.static('public'))
 
 
-
+// RUTAS MAIN
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/src/views/main/home.html')
 });
-
+// RUTAS PRODUCTOS
 app.get('/productos', (req,res)=>{
     res.sendFile(__dirname + '/src/views/products/listado.html')
 });
+app.get('/productos/crear', (req,res)=>{
+    res.sendFile(__dirname + '/src/views/products/create.html')
+});
+app.get('/productos/editar', (req,res)=>{
+    res.sendFile(__dirname + '/src/views/products/edit.html')
+});
 
+// RUTAS USUARIOS
 app.get('/usuarios/login', (req,res)=>{
     res.sendFile(__dirname + '/src/views/user/login.html')
 });
